0a23c6d40cb6f460474cf44c98935f5eda777a47
Larissa <larissaemini@gmail.com>
2025-07-17 09:59:22 -0300

    Segunda etapa: Classes Model e Interface Repository

---
+++ src/teste.ts
@@ -0,0 +1,16 @@
+import { ModaFitness } from "./model/ModaFitness";
+
+// Teste da Classe ModaFitness
+const produtoTeste = new ModaFitness(
+    1,
+    "Top Esportivo",
+    89.90,
+    "Top com sustentação para atividades físicas",
+    50,
+    "M",
+    "Preto",
+    "Top",
+    "Dry Fit"
+);
+
+produtoTeste.visualizar();
